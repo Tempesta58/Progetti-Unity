5be567561794d12feeca60aaa2526928204ecdc6
Giuseppe Arienzo <55873294+Tempesta58@users.noreply.github.com>
2020-09-08 16:36:03 +0200

    gdfgdf

---
+++ Esame/Arienzo_Giuseppe_6124/Assets/Caduto.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Caduto : MonoBehaviour
+{
+    public GameObject cil;
+    public Text txt;
+
+    private static int i;
+    private int flag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flag = 0;
+        i = 0;
+        txt.text = "Cilindri caduti = " + i;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((flag==0)&&(cil.GetComponent<Transform>().position.y < 0.5f))
+        {
+            i++;
+            flag = 1;
+            txt.text = "Cilindri caduti = " + i;
+            Destroy(cil);
+        }
+    }
+}
---
+++ Esame/Arienzo_Giuseppe_6124/Assets/Generatore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generatore : MonoBehaviour
+{
+    public GameObject cil;
+    private double tempo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            genera();
+        }
+
+        tempo += Time.deltaTime;
+
+        if (tempo > 5)
+        {
+            genera();
+            tempo = 0f;
+        }
+    }
+
+    public void genera()
+    {
+        Instantiate(cil, new Vector3(0.04f, 10.64f, 17.38f), new Quaternion(0, 0, 45, 45));
+    }
+}
